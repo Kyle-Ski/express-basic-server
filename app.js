fc619c7bc445b55c1e849e59272029b67c69fdac
Kyle Czajkowski <skiroyjenkins@gmail.com>
2018-10-22 11:46:18 -0600

    added error handling from Dans code in correct spots to handle errors

--- app.js
+++ app.js
@@ -10,10 +10,28 @@ app.get('/characters', (req, res, next) => {
 
 app.get('/characters/:id',(req, res, next) => {
     const id = req.params.id
-    res.json(characters[id-1])
+    if (!(Number(id))){
+        next()
+    }
+    const character = characters.filter(character => character.id == id)[0]
+    res.json({character: character})
 })
-app.listen(port, () => console.log(`I got you on http://localhost:${port}`))
 
+app.use(notFound)
+app.use(errorHandler)
+
+function notFound(req, res, next) {
+    res.status(404).send({error: 'Not found!', status: 404, url: req.originalUrl})
+}
+
+function errorHandler(err, req, res, next) {
+    console.error('ERROR', err)
+    const stack =  process.env.NODE_ENV !== 'production' ? err.stack : undefined
+    res.status(500).send({error: err.message, stack, url: req.originalUrl})
+}
+app.listen(port)
+    .on('error',     console.error.bind(console))
+    .on('listening', console.log.bind(console, 'I got you on http://localhost:' + port))
 // characters.map((character, i) => {
 //     return app.get(`/characters/${i+1}`, (req, res, next) => {
 //         res.json(character)
