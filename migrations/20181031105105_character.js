85b566b1a034ae13e7387af55304481133240534
Kyle Czajkowski <skiroyjenkins@gmail.com>
2018-10-31 11:16:20 -0600

    adding schema up and down in migrations

--- migrations/20181031105105_character.js
+++ migrations/20181031105105_character.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex, Promise) {
-
-};
+    knex.schema.createTable('character', function (table) {
+        table.increments()
+        table.string('name')
+        table.integer('height')
+        table.integer('mass')
+    })
+}
 
 exports.down = function(knex, Promise) {
-
-};
+    knex.schema.dropTableIfExist('character')
+}
