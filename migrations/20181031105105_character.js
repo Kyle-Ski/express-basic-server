be4f472710d248092a8a24b831341842ad4b50f9
Kyle Czajkowski <skiroyjenkins@gmail.com>
2018-10-31 11:20:15 -0600

    forgot the return commands in migrations

--- migrations/20181031105105_character.js
+++ migrations/20181031105105_character.js
@@ -1,6 +1,7 @@
 
+
 exports.up = function(knex, Promise) {
-    knex.schema.createTable('character', function (table) {
+    return knex.schema.createTable('character', function (table) {
         table.increments()
         table.string('name')
         table.integer('height')
@@ -9,5 +10,5 @@ exports.up = function(knex, Promise) {
 }
 
 exports.down = function(knex, Promise) {
-    knex.schema.dropTableIfExist('character')
+    return knex.schema.dropTableIfExist('character')
 }
